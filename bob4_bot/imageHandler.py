d205132ec0f810861cce4b03239df80da8a8984c
boba <bob4t1gd3@yandex.ru>
2020-06-07 01:15:01 +0300

    move swap folder

--- bob4_bot/imageHandler.py
+++ bob4_bot/imageHandler.py
@@ -1,15 +1,16 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputFile
 
+from swap_images.model_new import predict_brach
 from myHelpers import createInlineMenu
 import db
 
 async def imageHandler(message):
     res = db.findResult(message.from_user.id)
     if (res):
-        await message.photo[-1].download("users_images/" + str(message.from_user.id) + '.jpg')
-        await message.answer('кажется кто-то забыл привязать EORA api к боту...')
-        await message.answer_photo(InputFile("users_images/" + str(message.from_user.id) + '.jpg'), 'Забирай свою фотку и приходи когда разработчик вернётся из туалета')
+        img_path = "users_images/" + str(message.from_user.id) + '.jpg'
+        await message.photo[-1].download(img_path)
+        print(predict_brach(img_path))
 
     else:
         await message.answer('Рановато кидаешь фотку\nПройди тест!', reply_markup = createInlineMenu([
